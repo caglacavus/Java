874309c28c481eb68f76281b508b1746e9473a7d
caglacavus <caglaacavus@gmail.com>
2023-05-09 14:00:00 +0300

    Gun02 _02_Ornek1

---
+++ src/Gun02/_02_Ornek1.java
@@ -0,0 +1,13 @@
+package Gun02;
+
+public class _02_Ornek1 {
+    public static void main(String[] args) {
+
+
+        System.out.println("Çağla Çavuş"); // \n varsa yazdıktan sonra alt satıra geçer
+        System.out.println();              // yeni satır açar
+        System.out.println("Merhaba");
+        System.out.println();
+        System.out.println("Çağla \n Çavuş"); // \n -> yeni satır açar
+    }
+}
